24c2150d3d9e0a16376f5d5b4a2c9c3db402db65
Lucas <sennavdkoppel@gmail.com>
2025-06-11 12:48:01 +0200

    general nav css first draft

--- resources/views/workspaces/edit.blade.php
+++ resources/views/workspaces/edit.blade.php
@@ -1,16 +1,10 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Workspace bewerken</title>
-</head>
-<body>
-<h1>Bewerk workspace</h1>
-<form method="POST" action="{{ route('workspaces.update', $workspace) }}">
-    @csrf
-    @method('PUT')
-    <label for="name">Naam:</label>
-    <input type="text" name="name" value="{{ $workspace->name }}" required>
-    <button type="submit">Opslaan</button>
-</form>
-</body>
-</html>
+<x-nav>
+    <h1>Bewerk workspace</h1>
+    <form method="POST" action="{{ route('workspaces.update', $workspace) }}">
+        @csrf
+        @method('PUT')
+        <label for="name">Naam:</label>
+        <input type="text" name="name" value="{{ $workspace->name }}" required>
+        <button type="submit">Opslaan</button>
+    </form>
+</x-nav>
