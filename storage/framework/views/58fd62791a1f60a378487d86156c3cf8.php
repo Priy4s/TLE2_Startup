f99b7a64c391deb773c15ab0756d7a7677674883
Lucas <sennavdkoppel@gmail.com>
2025-06-19 11:41:48 +0200

    search + button css on docs

--- storage/framework/views/58fd62791a1f60a378487d86156c3cf8.php
+++ storage/framework/views/58fd62791a1f60a378487d86156c3cf8.php
@@ -14,8 +14,8 @@
             <div>
                 <form method="GET" action="<?php echo e(route('documents.overview')); ?>" class="mb-4 flex gap-2 items-center">
                     <input type="text" name="search" placeholder="Search documents..." value="<?php echo e(request('search')); ?>"
-                           class="border-gray-300 dark:border-gray-700 dark:bg-articleBlue dark:text-viridian focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm">
-                    <button type="submit" class="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700">Search</button>
+                           class="px-4 py-2 border-gray-300 dark:border-gray-700 dark:bg-articleBlue dark:text-viridian focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm w-[44vw]">
+                    <button type="submit" class="px-4 py-2 rounded-md bg-articleBlue hover:bg-accentGreen text-viridian hover:text-greenLight dark:bg-accentBlue dark:hover:bg-accentGreen dark:text-greenLight">Search</button>
                 </form>
 
                 
