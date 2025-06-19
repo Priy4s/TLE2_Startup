4376be649c315967109c7aa7e0c92fbc0c748d7f
Alex <alexdusselaar@gmail.com>
2025-06-19 14:40:41 +0200

    web.php fix

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\GoogleController;
 use App\Http\Controllers\DocumentController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\KikkermanController;
+use App\Http\Controllers\CalendarController;
 
 use App\Http\Controllers\MicrosoftController;
 
