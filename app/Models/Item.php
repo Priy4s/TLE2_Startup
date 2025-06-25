73478cc0ef3d3c59ea80efc40e4a6dc412cbd947
Priy4s <94791821+Priy4s@users.noreply.github.com>
2025-06-25 13:54:37 +0200

    fillable items nog een keer

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Item extends Model
 {
-    protected $fillable = ['id', 'name', 'category', 'image', 'created_at', 'updated_at'];
+    protected $fillable = ['id', 'name', 'category', 'image_path', 'created_at', 'updated_at'];
     public function users()
     {
         return $this->belongsToMany(User::class);
